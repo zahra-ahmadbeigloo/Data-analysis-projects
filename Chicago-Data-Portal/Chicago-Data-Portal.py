787f8fab6d7204a4eb50a5dddc1e584573c9a43f
Zahra Ahmadbeigloo <zahraahmadbeigloo@gmail.com>
2025-04-25 13:16:47 +0100

    Update and rename Chicago-Data-Portal.sql to Chicago-Data-Portal.py

---
+++ Chicago-Data-Portal/Chicago-Data-Portal.py
@@ -0,0 +1,12 @@
+import sqlite3, csv, pandas as pd
+connection = sqlite3.connect("FinalDB.db")
+cursor = connection.cursor()
+
+%load_ext sql
+
+df1 = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DB0201EN-SkillsNetwork/labs/FinalModule_Coursera_V5/data/ChicagoCensusData.csv?utm_medium=Exinfluencer&utm_source=Exinfluencer&utm_content=000026UJ&utm_term=10006555&utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkDB0201ENSkillsNetwork20127838-2021-01-01")
+df2 = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DB0201EN-SkillsNetwork/labs/FinalModule_Coursera_V5/data/ChicagoPublicSchools.csv?utm_medium=Exinfluencer&utm_source=Exinfluencer&utm_content=000026UJ&utm_term=10006555&utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkDB0201ENSkillsNetwork20127838-2021-01-01")
+df3 = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DB0201EN-SkillsNetwork/labs/FinalModule_Coursera_V5/data/ChicagoCrimeData.csv?utm_medium=Exinfluencer&utm_source=Exinfluencer&utm_content=000026UJ&utm_term=10006555&utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkDB0201ENSkillsNetwork20127838-2021-01-01")
+df1.to_sql("CENSUS_DATA", connection, if_exists = "replace", index = False, method = "multi")
+df2.to_sql("CHICAGO_PUBLIC_SCHOOLS", connection, if_exists = "replace", index = False, method = "multi")
+df3.to_sql("CHICAGO_CRIME_DATA", connection, if_exists = "replace", index = False, method = "multi")
